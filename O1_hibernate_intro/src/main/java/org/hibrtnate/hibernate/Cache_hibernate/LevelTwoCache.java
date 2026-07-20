49ea3db7395831ec1e98cbc347025f2a61ef674b
manoj <158544348+satheeshmaticz@users.noreply.github.com>
2026-07-20 18:10:36 +0530

    crus

--- O1_hibernate_intro/src/main/java/org/hibrtnate/hibernate/Cache_hibernate/LevelTwoCache.java
+++ O1_hibernate_intro/src/main/java/org/hibrtnate/hibernate/Cache_hibernate/LevelTwoCache.java
@@ -8,27 +8,30 @@ import org.hibrtnate.hibernate.model.Student;
 
 public class LevelTwoCache {
     public static void main(String[] args) {
-        SessionFactory sessionFactory= GetSessionFactory.getConnectedSessionFactory();
-        Session session=null;
-        Session session1=null;
-        try{
-            session=sessionFactory.openSession();
-            Student stu=session.find(Student.class,2);
-            System.out.println(stu.getName());
+        SessionFactory sessionFactory = GetSessionFactory.getConnectedSessionFactory();
+        Session session = null;
+        Session session1 = null;
+        try {
+            // First Session - Query 1: DB hit (L1 & L2 cache miss)
+            session = sessionFactory.openSession();
+            Student stu = session.find(Student.class, 2);
+            System.out.println("First Query (Session 1): " + stu.getName());
 
-            Student stu1=session.find(Student.class,2);
-            System.out.println(stu1.getName());
+            // First Session - Query 2: L1 cache hit (same session, same object)
+            Student stu1 = session.find(Student.class, 2);
+            System.out.println("Second Query (Session 1): " + stu1.getName());
 
-            session1=sessionFactory.openSession();
-            Student stu2=session1.find(Student.class,2);
-            System.out.println(stu2.getName());
+            // Second Session - Query 3: L2 cache hit (different session, but L2 cache shared)
+            session1 = sessionFactory.openSession();
+            Student stu2 = session1.find(Student.class, 2);
+            System.out.println("Third Query (Session 2): " + stu2.getName());
 
-        }catch(HibernateException exception){
+        } catch (HibernateException exception) {
             exception.printStackTrace();
-        }finally{
+        } finally {
+            if (session != null) session.close();
+            if (session1 != null) session1.close();
             sessionFactory.close();
-            session1.close();
-            session.close();
         }
     }
 }
