2acfd5e1f2911eb017be28cc710386f7d08db1aa
manoj <158544348+satheeshmaticz@users.noreply.github.com>
2026-07-23 15:31:52 +0530

    hibernate hql

--- 02_hibernate_maping/src/main/java/org/example/app/HQl/HqlOPerations.java
+++ 02_hibernate_maping/src/main/java/org/example/app/HQl/HqlOPerations.java
@@ -1,5 +1,7 @@
 package org.example.app.HQl;
 
+import org.example.CommonHooks.InputHooks;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.example.Model.Student;
 import org.example.db_hooks.GetSessionFactory;
@@ -12,7 +14,7 @@ import java.util.List;
 public class HqlOPerations {
     public static void main(String[] args) {
         HqlOPerations hqlOPerations=new HqlOPerations();
-        hqlOPerations.getAllStudent();
+        hqlOPerations.GetStudentOnlyName();
     }
 
     public void getAllStudent(){
@@ -28,6 +30,58 @@ public class HqlOPerations {
 
         }finally {
             session.close();
+        }
+    }
+
+    public void GetStudentByname(){
+        String name= InputHooks.getStringValue("Enter the student name");
+        Session session=null;
+
+        try{
+            session=GetSessionFactory.getSessionFactory().openSession();
+            Query<Student> query=session.createQuery("from Student where name = :stname",Student.class).setParameter("stname",name);
+            for(Student stu:query.list()){
+                System.out.println(stu);
+            }
+        }catch(HibernateException exception){
+            exception.printStackTrace();
+        }
+    }
+
+    public void GetStudentOnlyName(){
+        String name= InputHooks.getStringValue("Enter the student name");
+        Session session=null;
+
+        try{
+            session=GetSessionFactory.getSessionFactory().openSession();
+            Query<String> query=session.createQuery("select name from Student where name = :stname",String.class).setParameter("stname",name);
+            for(String stu:query.list()){
+                System.out.println(stu);
+            }
+        }catch(HibernateException exception){
+            exception.printStackTrace();
+        }
+    }
+
+    public void updateAlllist(){
+        Session session=null;
+        String college= InputHooks.getStringValue("Enter the student college");
+        String name= InputHooks.getStringValue("Enter the student name");
+        Transaction transaction=null;
+        try{
+            session=GetSessionFactory.getSessionFactory().openSession();
+            transaction=session.beginTransaction();
+            int rowsAffected=session.createQuery("update Student set college = :sclg where name = :stname and college != :scollege")
+                    .setParameter("stname",name)
+                    .setParameter("scollege",college)
+                    .setParameter("sclg",name)
+                    .executeUpdate();
+            transaction.commit();
+        }catch(HibernateException exception){
+            exception.printStackTrace();
+        }finally {
+            session.close();
+//            transact
         }
     }
 }
